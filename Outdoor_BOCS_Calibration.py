af8d7a2785a26cd8ddc2a7560c61097d6339ccf7
pbc504 <pbc504@york.ac.uk>
2019-07-23 17:06:20 +0100

    index=0 for df when reading file

--- Outdoor_BOCS_Calibration.py
+++ Outdoor_BOCS_Calibration.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 
-df = pd.read_csv("../aviva_april_2019.csv")
+df = pd.read_csv("../aviva_april_2019.csv", index_col=0)
 df2 = pd.read_csv("../bocs_aviva_raw_2019-03_2019-06/SENSOR_ARRAY_1_2019-04-01_data.log", index_col=0)
 df3 = pd.read_csv("../bocs_aviva_raw_2019-03_2019-06/SENSOR_ARRAY_2_2019-04-01_data.log", index_col=0)
 
