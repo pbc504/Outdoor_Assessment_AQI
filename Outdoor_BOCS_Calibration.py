ff48841d5a540ee1c49b9a28a5c010c10a329fb2
pbc504 <pbc504@york.ac.uk>
2019-07-23 16:15:06 +0100

    added file path

--- Outdoor_BOCS_Calibration.py
+++ Outdoor_BOCS_Calibration.py
@@ -1,9 +1,9 @@
 import numpy as np
 import pandas as pd
 
-df = pd.read_csv("aviva_april_2019.csv")
-df2 = pd.read_csv("SENSOR_ARRAY_1_2019-04-01_data.log", index_col=0)
-df3 = pd.read_csv("SENSOR_ARRAY_2_2019-04-01_data.log", index_col=0)
+df = pd.read_csv("../aviva_april_2019.csv")
+df2 = pd.read_csv("../bocs_aviva_raw_2019-03_2019-06/SENSOR_ARRAY_1_2019-04-01_data.log", index_col=0)
+df3 = pd.read_csv("../bocs_aviva_raw_2019-03_2019-06/SENSOR_ARRAY_2_2019-04-01_data.log", index_col=0)
 
 #a = df.describe(include=np.number)
 #with pd.option_context("display.max_rows", None, "display.max_columns", None):
@@ -13,4 +13,4 @@ df2.index = pd.to_datetime(df2.index, unit='s')
 df2r = df2.resample("5Min").mean()
 
 df3.index = pd.to_datetime(df3.index, unit='s')
-df3r = df3.resample("5Min"),mean()
+df3r = df3.resample("5Min").mean()
