9ad785fa3a1eeafb06bbe3ab9179282ca4dfe7fc
pbc504 <pbc504@york.ac.uk>
2019-07-25 11:58:31 +0100

    More regression models

--- NO_01-07_April.py
+++ NO_01-07_April.py
@@ -44,3 +44,123 @@ joint_2r = joint_2.resample("5min").mean()
 
 #Concat joint_1r and joint_2r
 big_df = pd.concat([joint_1r, joint_2r], axis=1, sort=False)  # Data of 1st week of April, both sensor arrays, every 5 min. [2016 rows x 132 columns]
+
+#=====================================================================================================================================================
+
+# Performs LinearRegression for x = 1045100_TEMP_41_Scaled and y = temperature
+x = df.iloc[:2016,32].values.reshape((-1,1))
+y = big_df.iloc[:,65].values
+model = LinearRegression()
+model.fit(x,y)
+r_sq = model.score(x,y)
+print("Regression of reference Temperature for sensor temperature")
+print("R^2 = ",r_sq)
+print("Intercept: ", model.intercept_)
+print("Slope: ", model.coef_)
+
+# Performs LinearRegression for x = 1045100_NO_29_Scaled and y = no_1(from sensor array 1)
+x2 = df.iloc[:2016,2].values.reshape((-1,1))
+y2 = big_df.iloc[:,17].values
+model2 = LinearRegression()
+model2.fit(x2,y2)
+r_sq2 = model2.score(x2,y2)
+print()
+print("Regression of reference NO for sensor no_1")
+print("R^2 = ",r_sq2)
+print("Intercept: ", model2.intercept_)
+print("Slope: ", model2.coef_)
+
+
+# Plots all NO sensor outputs against the reference NO value. x = 1045100_NO_29_Scaled and y = no_*
+plt.figure(1)
+xa = df.iloc[:2016,2].values.reshape((-1,1))
+ya = big_df.iloc[:,17].values
+plt.scatter(xa,ya, label="1_no1")
+yb = big_df.iloc[:,18].values
+plt.scatter(xa,yb, label="1_no2")
+yc = big_df.iloc[:,19].values
+plt.scatter(xa,yc, label="1_no3")
+yd = big_df.iloc[:,20].values
+plt.scatter(xa,yd, label="1_no4")
+ye = big_df.iloc[:,21].values
+plt.scatter(xa,yd, label="1_no5")
+yf = big_df.iloc[:,22].values
+plt.scatter(xa,yf, label="1_no6")
+yg = big_df.iloc[:,83].values
+plt.scatter(xa,yg, label="2_no1")
+yh = big_df.iloc[:,84].values
+plt.scatter(xa,yh, label="2_no2")
+yi = big_df.iloc[:,85].values
+plt.scatter(xa,yi, label="2_no3")
+yj = big_df.iloc[:,86].values
+plt.scatter(xa,yj, label="2_no4")
+yk = big_df.iloc[:,87].values
+plt.scatter(xa,yk, label="2_no5")
+yl = big_df.iloc[:,88].values
+plt.scatter(xa,yl, label="2_no6")
+plt.title("NO data 1st week of April")
+plt.xlabel("Reference NO concentration /ppb")
+plt.ylabel("NO sensor outputs")
+plt.legend()
+plt.show()
+
+
+# Performs LinearRegression for x = 1045100_NOx_30_Scaled and y = no_1(from sensor array 1)
+x3 = df.iloc[:2016,14].values.reshape((-1,1))
+y3 = big_df.iloc[:,17].values
+model3 = LinearRegression()
+model3.fit(x3,y3)
+r_sq3 = model3.score(x3,y3)
+print()
+print("Regression of reference NOx for sensor no_1")
+print("R^2 = ",r_sq3)
+print("Intercept: ", model3.intercept_)
+print("Slope: ", model3.coef_)
+
+# Performs LinearRegression for x = sensor temperature and y = no_1(from sensor array 1)
+x4 = big_df.iloc[:,65].values.reshape((-1,1))
+y4 = big_df.iloc[:,17].values
+model4 = LinearRegression()
+model4.fit(x4,y4)
+r_sq4 = model4.score(x4,y4)
+print()
+print("Regression of sensor temperature for sensor no_1")
+print("R^2 = ",r_sq4)
+print("Intercept: ", model4.intercept_)
+print("Slope: ", model4.coef_)
+
+# Performs LinearRegression for x = sensor humidity and y = no_1(from sensor array 1)
+x5 = big_df.iloc[:,64].values.reshape((-1,1))
+y5 = big_df.iloc[:,17].values
+model5 = LinearRegression()
+model5.fit(x5,y5)
+r_sq5 = model5.score(x5,y5)
+print()
+print("Regression of sensor humidity for sensor no_1")
+print("R^2 = ",r_sq5)
+print("Intercept: ", model5.intercept_)
+print("Slope: ", model5.coef_)
+
+#Performs Multiple LinearRegression with x = Scaled NO,NOx and sensor temp for y= no_1 (from sensor array 1)
+df_NO_NOx_temp = pd.concat([df.iloc[:2016,[2,14]].reindex(big_df.index),big_df.iloc[:,65]], axis=1, sort=False)
+x_new = df_NO_NOx_temp.values.reshape((-1,3))
+y_new = big_df.iloc[:,17].values
+model_new = LinearRegression().fit(x_new,y_new)
+r_sq_new = model_new.score(x_new,y_new)
+print()
+print("Regression of reference NO, reference NOx and sensor temp for sensor no_1")
+print("R^2 = ",r_sq_new)
+print("Intercept: ", model_new.intercept_)
+print("Slope: ", model_new.coef_)
+
+#Performs Multiple LinearRegression with x = Scaled NO,NOx, sensor temp and humidity for y= no_1 (from sensor array 1)
+df_NO_NOx_temp_hum = pd.concat([df.iloc[:2016,[2,14]].reindex(big_df.index),big_df.iloc[:,[64,65]]], axis=1, sort=False)
+x_new2 = df_NO_NOx_temp_hum.values.reshape((-1,4))
+y_new2 = big_df.iloc[:,17].values
+model_new2 = LinearRegression().fit(x_new2,y_new2)
+r_sq_new2 = model_new2.score(x_new2,y_new2)
+print()
+print("Regression of reference NO, reference NOx, sensor temp and sensor hum for sensor no_1")
+print("R^2 = ",r_sq_new2)
+print("Intercept: ", model_new2.intercept_)
+print("Slope: ", model_new2.coef_)
