99a3898cd99f20ad62fb3258f63027db6009602d
pbc504 <pbc504@york.ac.uk>
2019-07-29 17:25:18 +0100

    function to calculate linear regression

--- NO_April_all.py
+++ NO_April_all.py
@@ -20,21 +20,26 @@ for file in glob.glob("../bocs_aviva_raw_2019-03_2019-06/SENSOR_ARRAY_2_2019-04*
     df2_1r = df2_1.resample("5Min").mean()
     df2 = df2.append(df2_1r, sort=False)
 
-sensor_df = pd.concat([df1, df2], axis=1, sort=False)  # Data of April, both sensor arrays, every 5 min. [8640 rows x 132 columns]
+#sensor_df = pd.concat([df1, df2], axis=1, sort=False)  # Data of April, both sensor arrays, every 5 min. [8640 rows x 132 columns]
 
 #===================================================================================================================================
+# y = truth = scaled NO
+# x = predictor
 
-# Performs LinearRegression for x = 1045100_NO_29_Scaled and y = no_1(from sensor array 1)
-x2 = ref_df.iloc[:,2].values.reshape((-1,1))
-y2 = sensor_df.iloc[:,17].values
-model2 = LinearRegression()
-model2.fit(x2,y2)
-r_sq2 = model2.score(x2,y2)
-print()
-print("Regression of reference NO for sensor no_1")
-print("R^2 = ",r_sq2)
-print("Intercept: ", model2.intercept_)
-print("Slope: ", model2.coef_)
+# Function to calculate linear Regression
+def calculate_linear_regression(predictors, truth):
+    x = predictors.values
+    y = truth.values
+    model = LinearRegression().fit(x,y)
+    r_sq = model.score(x,y)
+    print()
+    print("R^2 = ",r_sq)
+    print("Intercept: ", model.intercept_)
+    print("Slope: ", model.coef_)
+
+
+# Performs LinearRegression for reference NO vs no_1 from sensor array 1
+calculate_linear_regression(df1[['no_1']], ref_df['1045100_NO_29_Scaled'])
 
 
 # Plots all NO sensor outputs against the reference NO value. x = 1045100_NO_29_Scaled and y = no_*
@@ -53,18 +58,9 @@ plt.ylabel("NO sensor outputs")
 plt.legend()
 plt.show()
 
-# Performs LinearRegression for x = 1045100_NOx_30_Scaled and y = no_1(from sensor array 1)
-x3 = ref_df.iloc[:,14].values.reshape((-1,1))
-y3 = sensor_df.iloc[:,17].values
-model3 = LinearRegression()
-model3.fit(x3,y3)
-r_sq3 = model3.score(x3,y3)
-print()
-print("Regression of reference NOx for sensor no_1")
-print("R^2 = ",r_sq3)
-print("Intercept: ", model3.intercept_)
-print("Slope: ", model3.coef_)
 
+# Perform LinearRegression for reference NOx vs no_1 from sensor array 1
+calculate_linear_regression(df1['no_1'], ref_df['1045100_NOx_30_Scaled'], 1)
 
 # Plots all NO sensor outputs against the reference NOx value. x = 1045100_NOx_30_Scaled and y = no_*
 plt.figure(2)
@@ -82,29 +78,14 @@ plt.ylabel("NO sensor outputs")
 plt.legend()
 plt.show()
 
-# Performs LinearRegression for x = sensor temperature and y = no_1(from sensor array 1)
-x4 = sensor_df.iloc[:,65].values.reshape((-1,1))
-y4 = sensor_df.iloc[:,17].values
-model4 = LinearRegression()
-model4.fit(x4,y4)
-r_sq4 = model4.score(x4,y4)
-print()
-print("Regression of sensor temperature for sensor no_1")
-print("R^2 = ",r_sq4)
-print("Intercept: ", model4.intercept_)
-print("Slope: ", model4.coef_)
+# Performs LinearRegression for bocs no_1 from sensor array 1 vs bocs temperature
+calculate_linear_regression(df1['temperature'], df1['no_1'], 1)
 
-# Performs LinearRegression for x = sensor humidity and y = no_1(from sensor array 1)
-x5 = sensor_df.iloc[:,64].values.reshape((-1,1))
-y5 = sensor_df.iloc[:,17].values
-model5 = LinearRegression()
-model5.fit(x5,y5)
-r_sq5 = model5.score(x5,y5)
-print()
-print("Regression of sensor humidity for sensor no_1")
-print("R^2 = ",r_sq5)
-print("Intercept: ", model5.intercept_)
-print("Slope: ", model5.coef_)
+# Performs LinearRegression for bocs no_1 from sensor array 1 vs bocs relative humidity
+calculate_linear_regression(df1['relative_humidity'], df1['no_1'], 1)
+
+# Performs Multiple regression for reference NO vs no_1 and temperature
+#calculate_linear_regression(df1[])
 
 #Performs Multiple LinearRegression with x = Scaled NO,NOx and sensor temp for y= no_1 (from sensor array 1)
 df_NO_NOx_temp = pd.concat([ref_df.iloc[:,[2,14]].reindex(sensor_df.index),sensor_df.iloc[:,65]], axis=1, sort=False)
