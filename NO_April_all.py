af5f5d10bb5084c54d38fe42d200029fc829cf5d
pbc504 <pbc504@york.ac.uk>
2019-07-30 11:24:36 +0100

    modified parts to use linear regression function

--- NO_April_all.py
+++ NO_April_all.py
@@ -20,11 +20,7 @@ for file in glob.glob("../bocs_aviva_raw_2019-03_2019-06/SENSOR_ARRAY_2_2019-04*
     df2_1r = df2_1.resample("5Min").mean()
     df2 = df2.append(df2_1r, sort=False)
 
-#sensor_df = pd.concat([df1, df2], axis=1, sort=False)  # Data of April, both sensor arrays, every 5 min. [8640 rows x 132 columns]
-
 #===================================================================================================================================
-# y = truth = scaled NO
-# x = predictor
 
 # Function to calculate linear Regression
 def calculate_linear_regression(predictors, truth):
@@ -41,89 +37,51 @@ def calculate_linear_regression(predictors, truth):
 # Performs LinearRegression for reference NO vs no_1 from sensor array 1
 calculate_linear_regression(df1[['no_1']], ref_df['1045100_NO_29_Scaled'])
 
-
-# Plots all NO sensor outputs against the reference NO value. x = 1045100_NO_29_Scaled and y = no_*
-plt.figure(1)
-x = ref_df.iloc[:,2].values.reshape((-1,1))
-for number in range(17,23):
-    y = sensor_df.iloc[:,number].values
-    plt.scatter(x,y, label = number)
-for number in range(83,89):
-    y = sensor_df.iloc[:,number].values
-    plt.scatter(x,y, label = number)
-plt.ylim(1400,1850)
-plt.title("NO data for April")
-plt.xlabel("Reference NO concentration /ppb")
-plt.ylabel("NO sensor outputs")
-plt.legend()
-plt.show()
-
-
 # Perform LinearRegression for reference NOx vs no_1 from sensor array 1
-calculate_linear_regression(df1['no_1'], ref_df['1045100_NOx_30_Scaled'], 1)
-
-# Plots all NO sensor outputs against the reference NOx value. x = 1045100_NOx_30_Scaled and y = no_*
-plt.figure(2)
-x = ref_df.iloc[:,14].values.reshape((-1,1))
-for number in range(17,23):
-    y = sensor_df.iloc[:,number].values
-    plt.scatter(x,y, label = number)
-for number in range(83,89):
-    y = sensor_df.iloc[:,number].values
-    plt.scatter(x,y, label = number)
-plt.ylim(1400,1850)
-plt.title("NO data for April")
-plt.xlabel("Reference NOx concentration /ppb")
-plt.ylabel("NO sensor outputs")
-plt.legend()
-plt.show()
+calculate_linear_regression(df1[['no_1']], ref_df['1045100_NOx_30_Scaled'])
 
 # Performs LinearRegression for bocs no_1 from sensor array 1 vs bocs temperature
-calculate_linear_regression(df1['temperature'], df1['no_1'], 1)
+calculate_linear_regression(df1[['temperature']], df1['no_1'])
+
+# Performs LinearRegression for bocs no_2 from sensor array 1 vs bocs temperature
+calculate_linear_regression(df1[['temperature']], df1['no_2'])
 
 # Performs LinearRegression for bocs no_1 from sensor array 1 vs bocs relative humidity
-calculate_linear_regression(df1['relative_humidity'], df1['no_1'], 1)
+calculate_linear_regression(df1[['relative_humidity']], df1['no_1'])
+
+# Performs LinearRegression for bocs no_2 from sensor array 1 vs bocs relative humidity
+calculate_linear_regression(df1[['relative_humidity']], df1['no_2'])
+
+# Performs Multiple regression for reference NO vs no_1 and no_2
+calculate_linear_regression(df1[['no_1', 'no_2']], ref_df['1045100_NO_29_Scaled'])
 
 # Performs Multiple regression for reference NO vs no_1 and temperature
-#calculate_linear_regression(df1[])
+calculate_linear_regression(df1[['no_1', 'temperature']], ref_df['1045100_NO_29_Scaled'])
 
-#Performs Multiple LinearRegression with x = Scaled NO,NOx and sensor temp for y= no_1 (from sensor array 1)
-df_NO_NOx_temp = pd.concat([ref_df.iloc[:,[2,14]].reindex(sensor_df.index),sensor_df.iloc[:,65]], axis=1, sort=False)
-x_new = df_NO_NOx_temp.values.reshape((-1,3))
-y_new = sensor_df.iloc[:,17].values
-model_new = LinearRegression().fit(x_new,y_new)
-r_sq_new = model_new.score(x_new,y_new)
-print()
-print("Regression of reference NO, reference NOx and sensor temp for sensor no_1")
-print("R^2 = ",r_sq_new)
-print("Intercept: ", model_new.intercept_)
-print("Slope: ", model_new.coef_)
+# Performs Multiple regression for reference NO vs no_1, no_2, relative humidity and temperature
+calculate_linear_regression(df1[['no_1', 'no_2', 'relative_humidity', 'temperature']], ref_df['1045100_NO_29_Scaled'])
 
+# Performs Multiple regression for reference NOx vs no_1, no_2, relative humidity and temperature
+calculate_linear_regression(df1[['no_1', 'no_2', 'relative_humidity', 'temperature']], ref_df['1045100_NOx_30_Scaled'])
 
-#Performs Multiple LinearRegression with x = Scaled NO,NO2,NOx and sensor temp,hum,flowrate for y= no_1 (from sensor array 1)
-df_6var = pd.concat([ref_df.iloc[:,[2,8,14]].reindex(sensor_df.index),sensor_df.iloc[:,[63,64,65]]], axis=1, sort=False)
-x_new = df_6var.values.reshape((-1,6))
-y_new = sensor_df.iloc[:,17].values
-model_new = LinearRegression().fit(x_new,y_new)
-r_sq_new = model_new.score(x_new,y_new)
-print()
-print("Regression of reference NO, NOx and NO2 and sensor temp, hum and Flowrate for sensor no_1")
-print("R^2 = ",r_sq_new)
-print("Intercept: ", model_new.intercept_)
-print("Slope: ", model_new.coef_)
+# Performs Multiple regression for reference NO vs no_1-6, relative humidity and temperature
+calculate_linear_regression(df1[['no_1', 'no_2', 'no_3', 'no_4', 'no_5', 'no_6', 'relative_humidity', 'temperature']], ref_df['1045100_NO_29_Scaled'])
+
+# Performs Multiple regression for reference NOx vs no_1, no_2, relative humidity and temperature from sensor array 2
+calculate_linear_regression(df2[['no_1', 'no_2', 'relative_humidity', 'temperature']], ref_df['1045100_NOx_30_Scaled'])
+
+# Performs Multiple regression for reference NO vs no_1-6, relative humidity and temperature from sensor array 2
+calculate_linear_regression(df2[['no_1', 'no_2', 'no_3', 'no_4', 'no_5', 'no_6', 'relative_humidity', 'temperature']], ref_df['1045100_NO_29_Scaled'])
+
+# Performs Multiple regression for reference NO vs all 12 no values from both sensor arrays, relative_humidity and temperature
+joint_NO = pd.concat([df1[['no_1', 'no_2', 'no_3', 'no_4', 'no_5', 'no_6', 'relative_humidity', 'temperature']], df2[['no_1', 'no_2', 'no_3', 'no_4', 'no_5', 'no_6']]], axis=1, sort=False)
+calculate_linear_regression(joint_NO, ref_df['1045100_NO_29_Scaled'])
 
 
-##Performs Multiple LinearRegression with x = Scaled NO,NOx and sensor temp, hum, flowrate for y= all no sensors. Stores all the results in a dataframe
-df_NO_NOx_temp = pd.concat([ref_df.iloc[:,[2,14]].reindex(sensor_df.index),sensor_df.iloc[:,[63,64,65]]], axis=1, sort=False)
-x= df_NO_NOx_temp.values.reshape((-1,5))
-results = pd.DataFrame(columns=['r_sq', 'slope', 'intercept'])
-for number in range (17,23):
-    y = sensor_df.iloc[:,number].values
-    model = LinearRegression().fit(x,y)
-    results = results.append({'r_sq': model.score(x,y), 'slope': model.coef_, 'intercept': model.intercept_}, ignore_index=True)
-for number in range (83,89):
-    y = sensor_df.iloc[:,number].values
-    model = LinearRegression().fit(x,y)
-    results = results.append({'r_sq': model.score(x,y), 'slope': model.coef_, 'intercept': model.intercept_}, ignore_index=True)
 
-print(results)
+for sensor in ('no_1', 'no_2', 'no_3', 'no_4', 'no_5', 'no_6'):
+    calculate_linear_regression(df1[[sensor, 'relative_humidity', 'temperature']], ref_df['1045100_NO_29_Scaled'])
+    calculate_linear_regression(df2[[sensor, 'relative_humidity', 'temperature']], ref_df['1045100_NO_29_Scaled'])
+
+#for combo in itertools.combinations(df1.columns, 2):
+#    ...:     print(combo)
