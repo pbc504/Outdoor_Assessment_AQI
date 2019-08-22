4398d1f13dcdd4ec86189686d2eb5fdd628274cf
pbc504 <pbc504@york.ac.uk>
2019-08-22 11:34:02 +0100

    function to plot model

--- best_bocs_model.py
+++ best_bocs_model.py
@@ -7,9 +7,12 @@ import matplotlib.pyplot as plt
 import itertools
 
 # Read files with evaluated models
-april_df = pd.read_csv("../bocs_aviva_evaluated_models_april_2019.csv", header=0, index_col=0)
-march_df = pd.read_csv("../bocs_aviva_evaluated_models_march_2019.csv", header=0, index_col=0)
-may_df = pd.read_csv("../bocs_aviva_evaluated_models_may_2019.csv", header=0, index_col=0)
+april_models = pd.read_csv("../bocs_aviva_evaluated_models_april_2019.csv", header=0, index_col=0)
+april_models.name = 'april_models'
+march_models = pd.read_csv("../bocs_aviva_evaluated_models_march_2019.csv", header=0, index_col=0)
+march_models.name = 'march_models'
+may_models = pd.read_csv("../bocs_aviva_evaluated_models_may_2019.csv", header=0, index_col=0)
+may_models.name = 'may_models'
 
 # Function to print models with evaluated r_sq over 0.9
 def best_models(dataframe, filename):
@@ -18,113 +21,79 @@ def best_models(dataframe, filename):
     z = x.append(y)
     z.to_csv(filename)
 
-best_models(april_df, "../bocs_aviva_best_models_april_2019.csv")
+best_models(april_models, "../bocs_aviva_best_models_april_2019.csv")
 best_april = pd.read_csv("../bocs_aviva_best_models_april_2019.csv", index_col=0)
-best_models(march_df, "../bocs_aviva_best_models_march_2019.csv")
+best_models(march_models, "../bocs_aviva_best_models_march_2019.csv")
 best_march = pd.read_csv("../bocs_aviva_best_models_march_2019.csv", index_col=0)
-best_models(may_df, "../bocs_aviva_best_models_may_2019.csv")
+best_models(may_models, "../bocs_aviva_best_models_may_2019.csv")
 best_may = pd.read_csv("../bocs_aviva_best_models_may_2019.csv", index_col=0)
 
-#===================================================================================
-# Load march and april data
 
-# Reads reference data for march
-march_ref_df = pd.read_csv("../preprocessed_aviva_march_2019.csv", header=0, index_col=0,
-dtype={'TimeBeginning': 'object', 'NO_Scaled': np.float64, 'NO2_Scaled': np.float64, 'NOx_Scaled': np.float64, 'O3_Scaled': np.float64, 'WD_Scaled': np.float64, 'TEMP_Scaled': np.float64, 'HUM_Scaled': np.float64, 'WINDMS_Scaled': np.float64})
+## Read test files
+march_april_ref = pd.read_csv('../joint_files/joint_aviva_march-april_ref_2019.csv', index_col=0)
+march_april_df1 = pd.read_csv('../joint_files/joint_aviva_march-april_df1_2019.csv', index_col=0)
+march_april_df2 = pd.read_csv('../joint_files/joint_aviva_march-april_df2_2019.csv', index_col=0)
 
-# Reads selected columns of each preprocessed file in march, resamples them to 5 minutes and appends them into a dataframe containing all march data.
-# Same thing for both sensor arrays
-march_df1 = pd.DataFrame()
+march_may_ref = pd.read_csv('../joint_files/joint_aviva_march-may_ref_2019.csv', index_col=0)
+march_may_df1 = pd.read_csv('../joint_files/joint_aviva_march-may_df1_2019.csv', index_col=0)
+march_may_df2 = pd.read_csv('../joint_files/joint_aviva_march-may_df2_2019.csv', index_col=0)
 
-for file in glob.glob("../preprocessed_bocs_aviva_raw_2019-03_2019-06/preprocessed_SENSOR_ARRAY_1_2019-03*"):
-    df1_1 = pd.read_csv(file, header=0, index_col=0,
-    usecols=['timestamp', 'VOC', 'NO', 'CO', 'Ox', 'NO2',
-#    'co2_1_active', 'co2_1_reference', 'co2_2_active', 'co2_2_reference', 'co2_3_active', 'co2_3_reference',
-    'humidity_in_percentage', 'temperature_in_celsius'],
-    dtype={'timestamp': np.int64, 'VOC': np.float64, 'NO': np.float64, 'CO': np.float64, 'Ox': np.float64, 'NO2': np.float64,
-#    'co2_1': np.float64, 'co2_2': np.float64, 'co2_3': np.float64, 'co2_4': np.float64, 'co2_5': np.float64, 'co2_6': np.float64,
-    'humidity_in_percentage': np.float64, 'temperature_in_celsius': np.float64})
-    df1_1.index = pd.to_datetime(df1_1.index, unit='s')
-    df1_1r = df1_1.resample("5Min").mean()
-    march_df1 = march_df1.append(df1_1r, sort=False)
+april_may_ref = pd.read_csv('../joint_files/joint_aviva_april-may_ref_2019.csv', index_col=0)
+april_may_df1 = pd.read_csv('../joint_files/joint_aviva_april-may_df1_2019.csv', index_col=0)
+april_may_df2 = pd.read_csv('../joint_files/joint_aviva_april-may_df2_2019.csv', index_col=0)
 
 
-march_df2 = pd.DataFrame()
-
-for file in glob.glob("../preprocessed_bocs_aviva_raw_2019-03_2019-06/preprocessed_SENSOR_ARRAY_2_2019-03*"):
-    df2_1 = pd.read_csv(file, header=0, index_col=0,
-    usecols=['timestamp', 'VOC', 'NO', 'CO', 'Ox', 'NO2',
-#    'co2_1_active', 'co2_1_reference', 'co2_2_active', 'co2_2_reference', 'co2_3_active', 'co2_3_reference',
-    'humidity_in_percentage', 'temperature_in_celsius'],
-    dtype={'timestamp': np.int64, 'VOC': np.float64, 'NO': np.float64, 'CO': np.float64, 'Ox': np.float64, 'NO2': np.float64,
-#    'co2_1': np.float64, 'co2_2': np.float64, 'co2_3': np.float64, 'co2_4': np.float64, 'co2_5': np.float64, 'co2_6': np.float64,
-    'humidity_in_percentage': np.float64, 'temperature_in_celsius': np.float64})
-    df2_1.index = pd.to_datetime(df2_1.index, unit='s')
-    df2_1r = df2_1.resample("5Min").mean()
-    march_df2 = march_df2.append(df2_1r, sort=False)
-
-# Match start and finish of datalog for march
-march_ref_df = march_ref_df[1:]
-march_diff_len_1 = len(march_df1) - len(march_ref_df)
-march_df1 = march_df1[march_diff_len_1:]
-march_diff_len_2 = len(march_df2) - len(march_ref_df)
-march_df2 = march_df2[march_diff_len_2:]
-
-
-
-# Reads reference data for april
-april_ref_df = pd.read_csv("../preprocessed_aviva_april_2019.csv", header=0, index_col=0,
-dtype={'TimeBeginning': 'object', 'NO_Scaled': np.float64, 'NO2_Scaled': np.float64, 'NOx_Scaled': np.float64, 'O3_Scaled': np.float64, 'WD_Scaled': np.float64, 'TEMP_Scaled': np.float64, 'HUM_Scaled': np.float64, 'WINDMS_Scaled': np.float64})
-
-# Reads selected columns of each preprocessed file in may, resamples them to 5 minutes and appends them into a dataframe containing all may data.
-# Same thing for both sensor arrays
-april_df1 = pd.DataFrame()
-
-for file in glob.glob("../preprocessed_bocs_aviva_raw_2019-03_2019-06/preprocessed_SENSOR_ARRAY_1_2019-04*"):
-    df1_1 = pd.read_csv(file, header=0, index_col=0,
-    usecols=['timestamp', 'VOC', 'NO', 'CO', 'Ox', 'NO2',
-#    'co2_1_active', 'co2_1_reference', 'co2_2_active', 'co2_2_reference', 'co2_3_active', 'co2_3_reference',
-    'humidity_in_percentage', 'temperature_in_celsius'],
-    dtype={'timestamp': np.int64, 'VOC': np.float64, 'NO': np.float64, 'CO': np.float64, 'Ox': np.float64, 'NO2': np.float64,
-#    'co2_1': np.float64, 'co2_2': np.float64, 'co2_3': np.float64, 'co2_4': np.float64, 'co2_5': np.float64, 'co2_6': np.float64,
-    'humidity_in_percentage': np.float64, 'temperature_in_celsius': np.float64})
-    df1_1.index = pd.to_datetime(df1_1.index, unit='s')
-    df1_1r = df1_1.resample("5Min").mean()
-    april_df1 = april_df1.append(df1_1r, sort=False)
-
-
-april_df2 = pd.DataFrame()
-
-for file in glob.glob("../preprocessed_bocs_aviva_raw_2019-03_2019-06/preprocessed_SENSOR_ARRAY_2_2019-04*"):
-    df2_1 = pd.read_csv(file, header=0, index_col=0,
-    usecols=['timestamp', 'VOC', 'NO', 'CO', 'Ox', 'NO2',
-#    'co2_1_active', 'co2_1_reference', 'co2_2_active', 'co2_2_reference', 'co2_3_active', 'co2_3_reference',
-    'humidity_in_percentage', 'temperature_in_celsius'],
-    dtype={'timestamp': np.int64, 'VOC': np.float64, 'NO': np.float64, 'CO': np.float64, 'Ox': np.float64, 'NO2': np.float64,
-#    'co2_1': np.float64, 'co2_2': np.float64, 'co2_3': np.float64, 'co2_4': np.float64, 'co2_5': np.float64, 'co2_6': np.float64,
-    'humidity_in_percentage': np.float64, 'temperature_in_celsius': np.float64})
-    df2_1.index = pd.to_datetime(df2_1.index, unit='s')
-    df2_1r = df2_1.resample("5Min").mean()
-    april_df2 = april_df2.append(df2_1r, sort=False)
-
-
-## Append march and april dataframes
-ma_ref_df = march_ref_df.append(april_ref_df, sort=False)
-ma_df1 = march_df1.append(april_df1, sort=False)
-ma_df2 = march_df2.append(april_df2, sort=False)
-
-#================================================================================
 
 # Plot model trained on may to predict O3_Scaled with humidity, temperature,CO, Ox and NO2
-fig = plt.figure(1)
-x = ma_ref_df.index
-y_ref = ma_ref_df['O3_Scaled']
-y_pred = ma_df1[may_df.loc[419, 'Predictor_1']]*may_df.loc[419, 'Slope_1'] + ma_df1[may_df.loc[419, 'Predictor_2']]*may_df.loc[419, 'Slope_2'] + ma_df1[may_df.loc[419, 'Predictor_3']]*may_df.loc[419, 'Slope_3'] + ma_df1[may_df.loc[419, 'Predictor_4']]*may_df.loc[419, 'Slope_4'] + ma_df1[may_df.loc[419, 'Predictor_5']]*may_df.loc[419, 'Slope_5'] + may_df.loc[419, 'Intercept']
-plt.scatter(x, y_ref, label='Reference Data')
-plt.scatter(x, y_pred, label='Predicted Data with may model')
-plt.xlabel('Time')
-plt.ylabel('O3 concentration /ppb')
-plt.title('R^2 = 0.945665')
-plt.legend()
-fig.savefig('../plot_may_df[419].png')
+#fig = plt.figure(1)
+#x = ma_ref_df.index
+#y_ref = ma_ref_df['O3_Scaled']
+#y_pred = ma_df1[may_df.loc[419, 'Predictor_1']]*may_df.loc[419, 'Slope_1'] + ma_df1[may_df.loc[419, 'Predictor_2']]*may_df.loc[419, 'Slope_2'] + ma_df1[may_df.loc[419, 'Predictor_3']]*may_df.loc[419, 'Slope_3'] + ma_df1[may_df.loc[419, 'Predictor_4']]*may_df.loc[419, 'Slope_4'] + ma_df1[may_df.loc[419, 'Predictor_5']]*may_df.loc[419, 'Slope_5'] + may_df.loc[419, 'Intercept']
+#plt.scatter(x, y_ref, label='Reference Data')
+#plt.scatter(x, y_pred, label='Predicted Data with may model')
+#plt.xlabel('Time')
+#plt.ylabel('O3 concentration /ppb')
+#plt.title('R^2 = 0.945665')
+#plt.legend()
+#fig.savefig('../plot_may_df[419].png')
 #plt.show()
+
+# Function to plot the overlap between reference/truth data and data predicted with model
+def plot_model(figure, ref_dataframe, model_dataframe, test_dataframe, model):
+    fig = plt.figure(figure)
+    x = ref_dataframe.index
+    y_ref = ref_dataframe[model_dataframe.loc[model, 'Truth']]
+    predictor_1 = model_dataframe.loc[model, 'Predictor_1']
+    predictor_2 = model_dataframe.loc[model, 'Predictor_2']
+    predictor_3 = model_dataframe.loc[model, 'Predictor_3']
+    predictor_4 = model_dataframe.loc[model, 'Predictor_4']
+    predictor_5 = model_dataframe.loc[model, 'Predictor_5']
+    predictor_6 = model_dataframe.loc[model, 'Predictor_6']
+    predictor_7 = model_dataframe.loc[model, 'Predictor_7']
+    if predictor_2 == '0':
+        y_pred = test_dataframe[predictor_1]*model_dataframe.loc[model, 'Slope_1'] + model_dataframe.loc[model, 'Intercept']
+    elif predictor_3 == '0':
+        y_pred = test_dataframe[predictor_1]*model_dataframe.loc[model, 'Slope_1'] + test_dataframe[predictor_2]*model_dataframe.loc[model, 'Slope_2'] + model_dataframe.loc[model, 'Intercept']
+    elif predictor_4 == '0':
+        y_pred = test_dataframe[predictor_1]*model_dataframe.loc[model, 'Slope_1'] + test_dataframe[predictor_2]*model_dataframe.loc[model, 'Slope_2'] + test_dataframe[predictor_3]*model_dataframe.loc[model, 'Slope_3'] + model_dataframe.loc[model, 'Intercept']
+    elif predictor_5 == '0':
+        y_pred = test_dataframe[predictor_1]*model_dataframe.loc[model, 'Slope_1'] + test_dataframe[predictor_2]*model_dataframe.loc[model, 'Slope_2'] + test_dataframe[predictor_3]*model_dataframe.loc[model, 'Slope_3'] + test_dataframe[predictor_4]*model_dataframe.loc[model, 'Slope_4'] + model_dataframe.loc[model, 'Intercept']
+    elif predictor_6 == '0':
+        y_pred = test_dataframe[predictor_1]*model_dataframe.loc[model, 'Slope_1'] + test_dataframe[predictor_2]*model_dataframe.loc[model, 'Slope_2'] + test_dataframe[predictor_3]*model_dataframe.loc[model, 'Slope_3'] + test_dataframe[predictor_4]*model_dataframe.loc[model, 'Slope_4'] + test_dataframe[predictor_5]*model_dataframe.loc[model, 'Slope_5'] + model_dataframe.loc[model, 'Intercept']
+    elif predictor_7 == '0':
+        y_pred = test_dataframe[predictor_1]*model_dataframe.loc[model, 'Slope_1'] + test_dataframe[predictor_2]*model_dataframe.loc[model, 'Slope_2'] + test_dataframe[predictor_3]*model_dataframe.loc[model, 'Slope_3'] + test_dataframe[predictor_4]*model_dataframe.loc[model, 'Slope_4'] + test_dataframe[predictor_5]*model_dataframe.loc[model, 'Slope_5'] + test_dataframe[predictor_6]*model_dataframe.loc[model, 'Slope_6'] + model_dataframe.loc[model, 'Intercept']
+    else:
+        y_pred = test_dataframe[predictor_1]*model_dataframe.loc[model, 'Slope_1'] + test_dataframe[predictor_2]*model_dataframe.loc[model, 'Slope_2'] + test_dataframe[predictor_3]*model_dataframe.loc[model, 'Slope_3'] + test_dataframe[predictor_4]*model_dataframe.loc[model, 'Slope_4'] + test_dataframe[predictor_5]*model_dataframe.loc[model, 'Slope_5'] + test_dataframe[predictor_6]*model_dataframe.loc[model, 'Slope_6'] + test_dataframe[predictor_7]*model_dataframe.loc[model, 'Slope_7'] + model_dataframe.loc[model, 'Intercept']
+    plt.scatter(x, y_ref, label='Reference Data')
+    plt.scatter(x, y_pred, label='Predicted Data')
+    plt.xlabel('Time')
+    plt.ylabel(model_dataframe.loc[model, 'Truth'] + ' concentration/ppb')
+    test_dataframe['predicting'] = y_pred
+    y_pred = test_dataframe[['predicting']]
+    model_r_sq = LinearRegression().fit(y_pred,y_ref).score(y_pred,y_ref)
+    plt.title('Model with R^2 = '+ str(model_r_sq))
+    plt.legend()
+    fig.savefig('../plot_' + model_dataframe.name + '_' + str(model) +'.png')
+
+
+plot_model(2, march_april_ref, may_models, march_april_df2, 262)
