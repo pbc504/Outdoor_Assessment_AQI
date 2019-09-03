ff118008417e3ce3be961bcaed42fe0cd33a26f2
pbc504 <pbc504@york.ac.uk>
2019-09-03 12:37:20 +0100

    argparse filepaths

--- create_bocs_model.py
+++ create_bocs_model.py
@@ -1,74 +1,18 @@
+'''
+Program to train models with preprocessed files.
+Start program in command line with:
+%run create_bocs_model.py "../preprocessed_aviva_april_2019.csv" "../preprocessed_bocs_aviva_raw_2019-03_2019-06/*2019-04*" "../bocs_aviva_trained_models_april_2019.csv"
+
+Problem in data file of 11/05/2019 and 12/05/2019. Not using those days
+'''
+
 import numpy as np
 import pandas as pd
-import glob
+import os
+import argparse
 from sklearn.linear_model import LinearRegression
-import matplotlib.pyplot as plt
 import itertools
 
-# Reads reference data for april
-ref_df = pd.read_csv("../preprocessed_aviva_april_2019.csv", header=0, index_col=0,
-dtype={'TimeBeginning': 'object', 'NO_Scaled': np.float64, 'NO2_Scaled': np.float64, 'NOx_Scaled': np.float64, 'O3_Scaled': np.float64, 'WD_Scaled': np.float64, 'TEMP_Scaled': np.float64, 'HUM_Scaled': np.float64, 'WINDMS_Scaled': np.float64})
-
-
-# Reads selected columns of each preprocessed file in april, resamples them to 5 minutes and appends them into a dataframe containing all april data.
-# Same thing for both sensor arrays
-df1 = pd.DataFrame()
-
-for file in glob.glob("../preprocessed_bocs_aviva_raw_2019-03_2019-06/preprocessed_SENSOR_ARRAY_1_2019-04*"):
-    df1_1 = pd.read_csv(file, header=0, index_col=0,
-    usecols=['timestamp', 'VOC', 'NO', 'CO', 'Ox', 'NO2',
-#    'co2_1_active', 'co2_1_reference', 'co2_2_active', 'co2_2_reference', 'co2_3_active', 'co2_3_reference',
-    'humidity_in_percentage', 'temperature_in_celsius'],
-    dtype={'timestamp': np.int64, 'VOC': np.float64, 'NO': np.float64, 'CO': np.float64, 'Ox': np.float64, 'NO2': np.float64,
-#    'co2_1': np.float64, 'co2_2': np.float64, 'co2_3': np.float64, 'co2_4': np.float64, 'co2_5': np.float64, 'co2_6': np.float64,
-    'humidity_in_percentage': np.float64, 'temperature_in_celsius': np.float64})
-    df1_1.index = pd.to_datetime(df1_1.index, unit='s')
-    df1_1r = df1_1.resample("5Min").mean()
-    df1 = df1.append(df1_1r, sort=False)
-
-df2 = pd.DataFrame()
-
-for file in glob.glob("../preprocessed_bocs_aviva_raw_2019-03_2019-06/preprocessed_SENSOR_ARRAY_2_2019-04*"):
-    df2_1 = pd.read_csv(file, header=0, index_col=0,
-    usecols=['timestamp', 'VOC', 'NO', 'CO', 'Ox', 'NO2',
-#    'co2_1_active', 'co2_1_reference', 'co2_2_active', 'co2_2_reference', 'co2_3_active', 'co2_3_reference',
-    'humidity_in_percentage', 'temperature_in_celsius'],
-    dtype={'timestamp': np.int64, 'VOC': np.float64, 'NO': np.float64, 'CO': np.float64, 'Ox': np.float64, 'NO2': np.float64,
-#    'co2_1': np.float64, 'co2_2': np.float64, 'co2_3': np.float64, 'co2_4': np.float64, 'co2_5': np.float64, 'co2_6': np.float64,
-    'humidity_in_percentage': np.float64, 'temperature_in_celsius': np.float64})
-    df2_1.index = pd.to_datetime(df2_1.index, unit='s')
-    df2_1r = df2_1.resample("5Min").mean()
-    df2 = df2.append(df2_1r, sort=False)
-
-#====================================================================================================================
-"""
-### To train on March data
-## Change part of filename from april to march in line 9 and 156
-## Change part of filename from 04 to 03 in line 17 and 31
-
-# Match start and finish of datalog for March
-ref_df = ref_df[1:]
-diff_len_1 = len(df1) - len(ref_df)
-df1 = df1[diff_len_1:]
-diff_len_2 = len(df2) - len(ref_df)
-df2 = df2[diff_len_2:]
-
-
-### To train on May data
-## Change part of filename from april to may in line 9 and 156
-## Change part of filename from 04 to 05 in line 17 and 31
-
-# Match start and finish of datalog for May
-# Remove 11th and 12th of may from reference data as was a problem on those files of raw data (10*288=2880) 14th day- 3456
-ref_df1 = ref_df[:2880]
-ref_df2 = ref_df[3456:-1]
-ref_df = ref_df1.append(ref_df2, sort=False)
-diff_len_1 = len(df1) - len(ref_df)
-df1 = df1[:-diff_len_1]
-diff_len_2 = len(df2) - len(ref_df)
-df2 = df2[:-diff_len_2]
-"""
-#===================================================================================================================================
 
 # Function to append results of linear regression for different combinations
 results = pd.DataFrame(columns=['Truth', 'Sensor_Array', 'Predictor_1', 'Predictor_2', 'Predictor_3', 'Predictor_4', 'Predictor_5', 'Predictor_6', 'Predictor_7', 'Intercept', 'Slope_1', 'Slope_2', 'Slope_3', 'Slope_4', 'Slope_5', 'Slope_6', 'Slope_7', 'r_sq'])
@@ -139,6 +83,74 @@ def combos_results(dataframe,combo, truth, results_df):
     return results_df.append({'Truth': truth, 'Sensor_Array': dataframe.name,
     'Predictor_1': predictor_1, 'Predictor_2': predictor_2, 'Predictor_3': predictor_3, 'Predictor_4': predictor_4, 'Predictor_5': predictor_5, 'Predictor_6': predictor_6, 'Predictor_7': predictor_7,
     'Intercept': model.intercept_, 'Slope_1': coefficient_1, 'Slope_2': coefficient_2, 'Slope_3': coefficient_3, 'Slope_4': coefficient_4, 'Slope_5': coefficient_5, 'Slope_6': coefficient_6, 'Slope_7': coefficient_7, 'r_sq': r_sq}, ignore_index=True)
+#=================================================================================================================================================================================================
+# Arguments to parse
+parser = argparse.ArgumentParser(description = 'Month to train models')
+parser.add_argument("reference_filepath", help='Input reference filepath to train models on. Example: "../preprocessed_aviva_april_2019.csv".')
+parser.add_argument("arrays_filepath", nargs='+', help='Input arrays filepath to train. Example: "../preprocessed_bocs_aviva_raw_2019-03_2019-06/preprocessed*2019-04*".')
+parser.add_argument("results_filepath", help='Input filename for the results file. Example: ../bocs_aviva_trained_models_april_2019.csv')
+args = parser.parse_args()
+
+# Separates sensor array 1 files from sensor array 2 files
+all_files = args.arrays_filepath
+array_1_files = all_files[:len(all_files)//2]
+array_2_files = all_files[len(all_files)//2:]
+
+
+
+# Reads reference data
+ref_df = pd.read_csv(args.reference_filepath, header=0, index_col=0,
+dtype={'TimeBeginning': 'object', 'NO_Scaled': np.float64, 'NO2_Scaled': np.float64, 'NOx_Scaled': np.float64, 'O3_Scaled': np.float64, 'WD_Scaled': np.float64, 'TEMP_Scaled': np.float64, 'HUM_Scaled': np.float64, 'WINDMS_Scaled': np.float64})
+
+
+# Reads selected columns of each preprocessed file, resamples them to 5 minutes and appends them into a dataframe containing all the data for that month.
+# Same thing for both sensor arrays
+df1 = pd.DataFrame()
+
+for file in array_1_files:
+    df1_1 = pd.read_csv(file, header=0, index_col=0,
+    usecols=['timestamp', 'VOC', 'NO', 'CO', 'Ox', 'NO2',
+#    'co2_1_active', 'co2_1_reference', 'co2_2_active', 'co2_2_reference', 'co2_3_active', 'co2_3_reference',
+    'humidity_in_percentage', 'temperature_in_celsius'],
+    dtype={'timestamp': np.int64, 'VOC': np.float64, 'NO': np.float64, 'CO': np.float64, 'Ox': np.float64, 'NO2': np.float64,
+#    'co2_1': np.float64, 'co2_2': np.float64, 'co2_3': np.float64, 'co2_4': np.float64, 'co2_5': np.float64, 'co2_6': np.float64,
+    'humidity_in_percentage': np.float64, 'temperature_in_celsius': np.float64})
+    df1_1.index = pd.to_datetime(df1_1.index, unit='s')
+    df1_1r = df1_1.resample("5Min").mean()
+    df1 = df1.append(df1_1r, sort=False)
+
+df2 = pd.DataFrame()
+
+for file in array_2_files:
+    df2_1 = pd.read_csv(file, header=0, index_col=0,
+    usecols=['timestamp', 'VOC', 'NO', 'CO', 'Ox', 'NO2',
+#    'co2_1_active', 'co2_1_reference', 'co2_2_active', 'co2_2_reference', 'co2_3_active', 'co2_3_reference',
+    'humidity_in_percentage', 'temperature_in_celsius'],
+    dtype={'timestamp': np.int64, 'VOC': np.float64, 'NO': np.float64, 'CO': np.float64, 'Ox': np.float64, 'NO2': np.float64,
+#    'co2_1': np.float64, 'co2_2': np.float64, 'co2_3': np.float64, 'co2_4': np.float64, 'co2_5': np.float64, 'co2_6': np.float64,
+    'humidity_in_percentage': np.float64, 'temperature_in_celsius': np.float64})
+    df2_1.index = pd.to_datetime(df2_1.index, unit='s')
+    df2_1r = df2_1.resample("5Min").mean()
+    df2 = df2.append(df2_1r, sort=False)
+
+
+
+# Match start and finish of datalog
+# Remove 11th and 12th of may from reference data as was a problem on those files of raw data (10*288=2880) 14th day- 3456
+if os.path.basename(args.reference_filepath) == 'preprocessed_aviva_may_2019.csv':
+    ref_df1 = ref_df[:2880]
+    ref_df2 = ref_df[3456:-1]
+    ref_df = ref_df1.append(ref_df2, sort=False)
+    diff_len_1 = len(df1) - len(ref_df)
+    df1 = df1[:-diff_len_1]
+    diff_len_2 = len(df2) - len(ref_df)
+    df2 = df2[:-diff_len_2]
+elif len(ref_df) != len(df1):
+    ref_df = ref_df[1:]
+    diff_len_1 = len(df1) - len(ref_df)
+    df1 = df1[diff_len_1:]
+    diff_len_2 = len(df2) - len(ref_df)
+    df2 = df2[diff_len_2:]
 
 
 
@@ -153,4 +165,4 @@ for dataframe in (df1,df2):
             results = combos_results(dataframe, combo, 'NOx_Scaled', results)
             results = combos_results(dataframe, combo, 'O3_Scaled', results)
 
-results.to_csv('../bocs_aviva_trained_models_april_2019.csv')
+results.to_csv(args.results_filepath)
