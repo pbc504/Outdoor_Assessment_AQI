07107bb0e7b6003d818869a74221c12bc17694e3
pbc504 <pbc504@york.ac.uk>
2019-09-05 11:18:02 +0100

    working matching dates function

--- combine_bocs_months_and_plot.py
+++ combine_bocs_months_and_plot.py
@@ -10,9 +10,83 @@ import matplotlib.pyplot as plt
 import itertools
 
 
+# Function to plot the overlap between reference/truth data and data predicted with model
+def plot_model(figure, ref_dataframe, model_dataframe, test_dataframe, model):
+    fig = plt.figure(figure)
+    x = ref_dataframe.index
+    y_ref = ref_dataframe[model_dataframe.loc[model, 'Truth']]
+    predictor_1 = model_dataframe.loc[model, 'Predictor_1']
+    predictor_2 = model_dataframe.loc[model, 'Predictor_2']
+    predictor_3 = model_dataframe.loc[model, 'Predictor_3']
+    predictor_4 = model_dataframe.loc[model, 'Predictor_4']
+    predictor_5 = model_dataframe.loc[model, 'Predictor_5']
+    predictor_6 = model_dataframe.loc[model, 'Predictor_6']
+    predictor_7 = model_dataframe.loc[model, 'Predictor_7']
+    if predictor_2 == '0':
+        y_pred = test_dataframe[predictor_1]*model_dataframe.loc[model, 'Slope_1'] + model_dataframe.loc[model, 'Intercept']
+    elif predictor_3 == '0':
+        y_pred = test_dataframe[predictor_1]*model_dataframe.loc[model, 'Slope_1'] + test_dataframe[predictor_2]*model_dataframe.loc[model, 'Slope_2'] + model_dataframe.loc[model, 'Intercept']
+    elif predictor_4 == '0':
+        y_pred = test_dataframe[predictor_1]*model_dataframe.loc[model, 'Slope_1'] + test_dataframe[predictor_2]*model_dataframe.loc[model, 'Slope_2'] + test_dataframe[predictor_3]*model_dataframe.loc[model, 'Slope_3'] + model_dataframe.loc[model, 'Intercept']
+    elif predictor_5 == '0':
+        y_pred = test_dataframe[predictor_1]*model_dataframe.loc[model, 'Slope_1'] + test_dataframe[predictor_2]*model_dataframe.loc[model, 'Slope_2'] + test_dataframe[predictor_3]*model_dataframe.loc[model, 'Slope_3'] + test_dataframe[predictor_4]*model_dataframe.loc[model, 'Slope_4'] + model_dataframe.loc[model, 'Intercept']
+    elif predictor_6 == '0':
+        y_pred = test_dataframe[predictor_1]*model_dataframe.loc[model, 'Slope_1'] + test_dataframe[predictor_2]*model_dataframe.loc[model, 'Slope_2'] + test_dataframe[predictor_3]*model_dataframe.loc[model, 'Slope_3'] + test_dataframe[predictor_4]*model_dataframe.loc[model, 'Slope_4'] + test_dataframe[predictor_5]*model_dataframe.loc[model, 'Slope_5'] + model_dataframe.loc[model, 'Intercept']
+    elif predictor_7 == '0':
+        y_pred = test_dataframe[predictor_1]*model_dataframe.loc[model, 'Slope_1'] + test_dataframe[predictor_2]*model_dataframe.loc[model, 'Slope_2'] + test_dataframe[predictor_3]*model_dataframe.loc[model, 'Slope_3'] + test_dataframe[predictor_4]*model_dataframe.loc[model, 'Slope_4'] + test_dataframe[predictor_5]*model_dataframe.loc[model, 'Slope_5'] + test_dataframe[predictor_6]*model_dataframe.loc[model, 'Slope_6'] + model_dataframe.loc[model, 'Intercept']
+    else:
+        y_pred = test_dataframe[predictor_1]*model_dataframe.loc[model, 'Slope_1'] + test_dataframe[predictor_2]*model_dataframe.loc[model, 'Slope_2'] + test_dataframe[predictor_3]*model_dataframe.loc[model, 'Slope_3'] + test_dataframe[predictor_4]*model_dataframe.loc[model, 'Slope_4'] + test_dataframe[predictor_5]*model_dataframe.loc[model, 'Slope_5'] + test_dataframe[predictor_6]*model_dataframe.loc[model, 'Slope_6'] + test_dataframe[predictor_7]*model_dataframe.loc[model, 'Slope_7'] + model_dataframe.loc[model, 'Intercept']
+    plt.scatter(x, y_ref, label='Reference Data')
+    plt.scatter(x, y_pred, label='Predicted Data')
+    plt.xlabel('Time')
+    if (test_dataframe.name == march_april_df1.name) or (test_dataframe.name == march_april_df2.name):
+        plt.xticks([116, 2132, 4148, 6164, 8180, 10196, 12212, 14228], ['7/03/19', '14/03/19', '21/03/19', '28/03/19', '04/04/19', '11/04/19', '18/04/19', '25/04/19'], rotation=30)
+    elif (test_dataframe.name == march_may_df1.name) or (test_dataframe.name == march_may_df2.name):
+        plt.xticks([116, 2132, 4148, 6164, 8180, 10196, 12212, 14228], ['7/03/19', '14/03/19', '21/03/19', '28/03/19', '04/05/19', '13/05/19', '20/05/19', '27/05/19'], rotation=30)
+    elif (test_dataframe.name == april_may_df1.name) or (test_dataframe.name == april_may_df2.name):
+        plt.xticks([0, 2016, 4032, 6048, 8064, 10080, 12096, 14112, 16128], ['1/04/19', '8/04/19', '15/04/19', '22/04/19', '29/04/19', '6/05/19', '15/05/19', '22/05/19', '29/05/19'], rotation=30)
+    plt.ylabel(model_dataframe.loc[model, 'Truth'] + ' concentration /ppb')
+    test_dataframe['predicting'] = y_pred
+    y_pred = test_dataframe[['predicting']]
+    model_r_sq = LinearRegression().fit(y_pred,y_ref).score(y_pred,y_ref)
+    model_r_sq = format(model_r_sq, '.4f')
+    plt.title(model_dataframe.name + ' evaluated on ' + test_dataframe.name + '\n with R^2 = '+ str(model_r_sq))
+    plt.legend()
+    fig.savefig('../plot_' + model_dataframe.name + '_' + str(model) +'.png')
+
+
+# Function to match reference to bocs_data dates
+def match_dates(reference_dataframe, array1_dataframe, array2_dataframe):
+    first_date = max(reference_dataframe.index[0], array1_dataframe.index[0], array2_dataframe.index[0])
+    last_date = min(reference_dataframe.index[-1], array1_dataframe.index[-1], array2_dataframe.index[-1])
+    reference_dataframe = reference_dataframe[first_date:last_date]
+    array1_dataframe = array1_dataframe[first_date:last_date]
+    array2_dataframe = array2_dataframe[first_date:last_date]
+    if len(reference_dataframe) != len(array1_dataframe) or len(reference_dataframe) != len(array2_dataframe):
+        min_length = min(len(reference_dataframe), len(array1_dataframe), len(array2_dataframe))
+        if len(reference_dataframe) == min_length:
+            diff_1 = list(set(array1_dataframe.index) - set(reference_dataframe.index))
+            array1_dataframe = array1_dataframe.drop(diff_1)
+            diff_2 = list(set(array2_dataframe.index) - set(reference_dataframe.index))
+            array1_dataframe = array2_dataframe.drop(diff_2)
+        elif len(array1_dataframe) == min_length:
+            diff_1 = list(set(reference_dataframe.index) - set(array1_dataframe.index))
+            reference_dataframe = reference_dataframe.drop(diff_1)
+            diff_2 = list(set(array2_dataframe.index) - set(array1_dataframe.index))
+            array2_dataframe = array2_dataframe.drop(diff_2)
+        elif len(array2_dataframe) == min_length:
+            diff_1 = list(set(reference_dataframe.index) - set(array2_dataframe.index))
+            reference_dataframe = reference_dataframe.drop(diff_1)
+            diff_2 = list(set(array1_dataframe.index) - set(array2_dataframe.index))
+            array1_dataframe = array1_dataframe.drop(diff_2)
+    return reference_dataframe, array1_dataframe, array2_dataframe
+
+#===================================================================================
+
 # Reads reference data for march
 march_ref_df = pd.read_csv("../preprocessed_aviva_march_2019.csv", header=0, index_col=0,
 dtype={'TimeBeginning': 'object', 'NO_Scaled': np.float64, 'NO2_Scaled': np.float64, 'NOx_Scaled': np.float64, 'O3_Scaled': np.float64, 'WD_Scaled': np.float64, 'TEMP_Scaled': np.float64, 'HUM_Scaled': np.float64, 'WINDMS_Scaled': np.float64})
+march_ref_df.index = pd.to_datetime(march_ref_df.index)
 
 # Reads selected columns of each preprocessed file in march, resamples them to 5 minutes and appends them into a dataframe containing all march data.
 # Same thing for both sensor arrays
@@ -46,11 +120,9 @@ for file in glob.glob("../preprocessed_bocs_aviva_raw_2019-03_2019-06/preprocess
     march_df2 = march_df2.append(df2_1r, sort=False)
 
 # Match start and finish of datalog for march
-march_ref_df = march_ref_df[1:]
-march_diff_len_1 = len(march_df1) - len(march_ref_df)
-march_df1 = march_df1[march_diff_len_1:]
-march_diff_len_2 = len(march_df2) - len(march_ref_df)
-march_df2 = march_df2[march_diff_len_2:]
+march_ref_df = match_dates(march_ref_df, march_df1, march_df2)[0]
+march_df1 = match_dates(march_ref_df, march_df1, march_df2)[1]
+march_df2 = match_dates(march_ref_df, march_df1, march_df2)[2]
 
 # Write march dataframes to csv
 march_ref_df.to_csv('../joint_files/joint_aviva_march_ref_2019.csv')
@@ -62,6 +134,7 @@ march_df2.to_csv('../joint_files/joint_aviva_march_df2_2019.csv')
 # Reads reference data for april
 april_ref_df = pd.read_csv("../preprocessed_aviva_april_2019.csv", header=0, index_col=0,
 dtype={'TimeBeginning': 'object', 'NO_Scaled': np.float64, 'NO2_Scaled': np.float64, 'NOx_Scaled': np.float64, 'O3_Scaled': np.float64, 'WD_Scaled': np.float64, 'TEMP_Scaled': np.float64, 'HUM_Scaled': np.float64, 'WINDMS_Scaled': np.float64})
+april_ref_df.index = pd.to_datetime(april_ref_df.index)
 
 # Reads selected columns of each preprocessed file in may, resamples them to 5 minutes and appends them into a dataframe containing all may data.
 # Same thing for both sensor arrays
@@ -94,6 +167,11 @@ for file in glob.glob("../preprocessed_bocs_aviva_raw_2019-03_2019-06/preprocess
     df2_1r = df2_1.resample("5Min").mean()
     april_df2 = april_df2.append(df2_1r, sort=False)
 
+# Match start and finish of datalog for april
+april_ref_df = match_dates(april_ref_df, april_df1, april_df2)[0]
+april_df1 = match_dates(april_ref_df, april_df1, april_df2)[1]
+april_df2 = match_dates(april_ref_df, april_df1, april_df2)[2]
+
 # Write april dataframes to csv
 april_ref_df.to_csv('../joint_files/joint_aviva_april_ref_2019.csv')
 april_df1.to_csv('../joint_files/joint_aviva_april_df1_2019.csv')
@@ -104,6 +182,7 @@ april_df2.to_csv('../joint_files/joint_aviva_april_df2_2019.csv')
 # Reads reference data for may
 may_ref_df = pd.read_csv("../preprocessed_aviva_may_2019.csv", header=0, index_col=0,
 dtype={'TimeBeginning': 'object', 'NO_Scaled': np.float64, 'NO2_Scaled': np.float64, 'NOx_Scaled': np.float64, 'O3_Scaled': np.float64, 'WD_Scaled': np.float64, 'TEMP_Scaled': np.float64, 'HUM_Scaled': np.float64, 'WINDMS_Scaled': np.float64})
+may_ref_df.index = pd.to_datetime(may_ref_df.index)
 
 # Reads selected columns of each preprocessed file in may, resamples them to 5 minutes and appends them into a dataframe containing all may data.
 # Same thing for both sensor arrays
@@ -139,14 +218,9 @@ for file in glob.glob("../preprocessed_bocs_aviva_raw_2019-03_2019-06/preprocess
 
 ## Match start and finish of datalog for may
 ## Remove 11th and 12th of may from reference data as was a problem on those files of raw data (10*288=2880) 14th day- 3456
-may_ref_df1 = may_ref_df[:2880]
-may_ref_df2 = may_ref_df[3456:-1]
-may_ref_df = may_ref_df1.append(may_ref_df2, sort=False)
-may_diff_len_1 = len(may_df1) - len(may_ref_df)
-may_df1 = may_df1[:-may_diff_len_1]
-may_diff_len_2 = len(may_df2) - len(may_ref_df)
-may_df2 = may_df2[:-may_diff_len_2]
-
+may_ref_df = match_dates(may_ref_df, may_df1, may_df2)[0]
+may_df1 = match_dates(may_ref_df, may_df1, may_df2)[1]
+may_df2 = match_dates(may_ref_df, may_df1, may_df2)[2]
 
 # Write may dataframes to csv
 may_ref_df.to_csv('../joint_files/joint_aviva_may_ref_2019.csv')
@@ -197,53 +271,6 @@ may_models.name = 'may_model'
 
 # Sort models from march by highest r^sq evaluated on data from second array
 march_models.sort_values(['df2_evaluated_r_sq'], ascending=[False]).head(10)
-
-
-
-# Function to plot the overlap between reference/truth data and data predicted with model
-def plot_model(figure, ref_dataframe, model_dataframe, test_dataframe, model):
-    fig = plt.figure(figure)
-    x = ref_dataframe.index
-    y_ref = ref_dataframe[model_dataframe.loc[model, 'Truth']]
-    predictor_1 = model_dataframe.loc[model, 'Predictor_1']
-    predictor_2 = model_dataframe.loc[model, 'Predictor_2']
-    predictor_3 = model_dataframe.loc[model, 'Predictor_3']
-    predictor_4 = model_dataframe.loc[model, 'Predictor_4']
-    predictor_5 = model_dataframe.loc[model, 'Predictor_5']
-    predictor_6 = model_dataframe.loc[model, 'Predictor_6']
-    predictor_7 = model_dataframe.loc[model, 'Predictor_7']
-    if predictor_2 == '0':
-        y_pred = test_dataframe[predictor_1]*model_dataframe.loc[model, 'Slope_1'] + model_dataframe.loc[model, 'Intercept']
-    elif predictor_3 == '0':
-        y_pred = test_dataframe[predictor_1]*model_dataframe.loc[model, 'Slope_1'] + test_dataframe[predictor_2]*model_dataframe.loc[model, 'Slope_2'] + model_dataframe.loc[model, 'Intercept']
-    elif predictor_4 == '0':
-        y_pred = test_dataframe[predictor_1]*model_dataframe.loc[model, 'Slope_1'] + test_dataframe[predictor_2]*model_dataframe.loc[model, 'Slope_2'] + test_dataframe[predictor_3]*model_dataframe.loc[model, 'Slope_3'] + model_dataframe.loc[model, 'Intercept']
-    elif predictor_5 == '0':
-        y_pred = test_dataframe[predictor_1]*model_dataframe.loc[model, 'Slope_1'] + test_dataframe[predictor_2]*model_dataframe.loc[model, 'Slope_2'] + test_dataframe[predictor_3]*model_dataframe.loc[model, 'Slope_3'] + test_dataframe[predictor_4]*model_dataframe.loc[model, 'Slope_4'] + model_dataframe.loc[model, 'Intercept']
-    elif predictor_6 == '0':
-        y_pred = test_dataframe[predictor_1]*model_dataframe.loc[model, 'Slope_1'] + test_dataframe[predictor_2]*model_dataframe.loc[model, 'Slope_2'] + test_dataframe[predictor_3]*model_dataframe.loc[model, 'Slope_3'] + test_dataframe[predictor_4]*model_dataframe.loc[model, 'Slope_4'] + test_dataframe[predictor_5]*model_dataframe.loc[model, 'Slope_5'] + model_dataframe.loc[model, 'Intercept']
-    elif predictor_7 == '0':
-        y_pred = test_dataframe[predictor_1]*model_dataframe.loc[model, 'Slope_1'] + test_dataframe[predictor_2]*model_dataframe.loc[model, 'Slope_2'] + test_dataframe[predictor_3]*model_dataframe.loc[model, 'Slope_3'] + test_dataframe[predictor_4]*model_dataframe.loc[model, 'Slope_4'] + test_dataframe[predictor_5]*model_dataframe.loc[model, 'Slope_5'] + test_dataframe[predictor_6]*model_dataframe.loc[model, 'Slope_6'] + model_dataframe.loc[model, 'Intercept']
-    else:
-        y_pred = test_dataframe[predictor_1]*model_dataframe.loc[model, 'Slope_1'] + test_dataframe[predictor_2]*model_dataframe.loc[model, 'Slope_2'] + test_dataframe[predictor_3]*model_dataframe.loc[model, 'Slope_3'] + test_dataframe[predictor_4]*model_dataframe.loc[model, 'Slope_4'] + test_dataframe[predictor_5]*model_dataframe.loc[model, 'Slope_5'] + test_dataframe[predictor_6]*model_dataframe.loc[model, 'Slope_6'] + test_dataframe[predictor_7]*model_dataframe.loc[model, 'Slope_7'] + model_dataframe.loc[model, 'Intercept']
-    plt.scatter(x, y_ref, label='Reference Data')
-    plt.scatter(x, y_pred, label='Predicted Data')
-    plt.xlabel('Time')
-    if (test_dataframe.name == march_april_df1.name) or (test_dataframe.name == march_april_df2.name):
-        plt.xticks([116, 2132, 4148, 6164, 8180, 10196, 12212, 14228], ['7/03/19', '14/03/19', '21/03/19', '28/03/19', '04/04/19', '11/04/19', '18/04/19', '25/04/19'], rotation=30)
-    elif (test_dataframe.name == march_may_df1.name) or (test_dataframe.name == march_may_df2.name):
-        plt.xticks([116, 2132, 4148, 6164, 8180, 10196, 12212, 14228], ['7/03/19', '14/03/19', '21/03/19', '28/03/19', '04/05/19', '13/05/19', '20/05/19', '27/05/19'], rotation=30)
-    elif (test_dataframe.name == april_may_df1.name) or (test_dataframe.name == april_may_df2.name):
-        plt.xticks([0, 2016, 4032, 6048, 8064, 10080, 12096, 14112, 16128], ['1/04/19', '8/04/19', '15/04/19', '22/04/19', '29/04/19', '6/05/19', '15/05/19', '22/05/19', '29/05/19'], rotation=30)
-    plt.ylabel(model_dataframe.loc[model, 'Truth'] + ' concentration /ppb')
-    test_dataframe['predicting'] = y_pred
-    y_pred = test_dataframe[['predicting']]
-    model_r_sq = LinearRegression().fit(y_pred,y_ref).score(y_pred,y_ref)
-    model_r_sq = format(model_r_sq, '.4f')
-    plt.title(model_dataframe.name + ' evaluated on ' + test_dataframe.name + '\n with R^2 = '+ str(model_r_sq))
-    plt.legend()
-    fig.savefig('../plot_' + model_dataframe.name + '_' + str(model) +'.png')
-
 
 
 # Plot different models
